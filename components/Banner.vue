e849df99ff8ebd5f603742d02f53498a8207886e
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-01-26 16:45:06 +0800

    added new linking

--- components/Banner.vue
+++ components/Banner.vue
@@ -179,30 +179,33 @@ const { isLight, dashboard } = defineProps(["isLight", "dashboard"]);
                   </div>
                 </div>
               </a>
-              <a href="/solutions/indexes">
-                <div class="flex flex-row items-center gap-2">
-                  <svg
-                    width="26"
-                    height="16"
-                    viewBox="0 0 26 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div class="flex flex-row items-center gap-2">
+                <svg
+                  width="26"
+                  height="16"
+                  viewBox="0 0 26 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M14.3333 1.3335H25M25 1.3335V12.0002M25 1.3335L14.3333 12.0002L9 6.66683L1 14.6668"
+                    stroke="#181818"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <div>
+                  <p class="font-semibold">Indexes</p>
+                  <p class="text-gray-700 text-md font-light">
+                    Premium Independent Data
+                  </p>
+                  <p
+                    class="mt-1 text-white text-sm py-1 w-fit rounded-lg px-2 bg-truflation-500"
                   >
-                    <path
-                      d="M14.3333 1.3335H25M25 1.3335V12.0002M25 1.3335L14.3333 12.0002L9 6.66683L1 14.6668"
-                      stroke="#181818"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                  <div>
-                    <p class="font-semibold">Indexes</p>
-                    <p class="text-gray-700 text-md font-light">
-                      Premium Independent Data
-                    </p>
-                  </div>
+                    Coming Soon!
+                  </p>
                 </div>
-              </a>
+              </div>
               <a href="/solutions/widget">
                 <div class="flex flex-row items-center gap-2">
                   <svg
@@ -317,27 +320,6 @@ const { isLight, dashboard } = defineProps(["isLight", "dashboard"]);
           </ul>
         </li>
       </ul>
-    </div>
-    <div class="">
-      <a href="/">
-        <button :class="{ hidden: dashboard }" class="btn invisible sm:visible">
-          Dashboard
-          <svg
-            width="13"
-            height="13"
-            viewBox="0 0 13 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M6.73441 1.13275C7.04683 0.820333 7.55336 0.820333 7.86578 1.13275L12.6658 5.93275C12.9782 6.24517 12.9782 6.7517 12.6658 7.06412L7.86578 11.8641C7.55336 12.1765 7.04683 12.1765 6.73441 11.8641C6.42199 11.5517 6.42199 11.0452 6.73441 10.7328L10.1687 7.29844L0.900098 7.29844C0.458271 7.29844 0.100098 6.94026 0.100098 6.49844C0.100098 6.05661 0.458271 5.69844 0.900098 5.69844H10.1687L6.73441 2.26412C6.42199 1.9517 6.42199 1.44517 6.73441 1.13275Z"
-              fill="white"
-            />
-          </svg>
-        </button>
-      </a>
     </div>
   </header>
 </template>
