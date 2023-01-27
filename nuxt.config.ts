f68b52570c332fdcb024c42b7d3ce2fc503b47df
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-01-27 08:05:56 +0800

    added env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/content"],
+  runtimeConfig: {
+    // Private config that is only available on the server
+    apiSecret: process.env.GHOST_CONTENT_API_KEY,
+    // Config within public will be also exposed to the client
+    public: {
+      apiBase: "/api",
+    },
+  },
 });
--- server/api/ghostPosts.js
+++ server/api/ghostPosts.js
@@ -1,12 +1,14 @@
 import GhostContentAPI from "@tryghost/content-api";
 
-const api = new GhostContentAPI({
-  url: "https://truflation.ghost.io",
-  key: process.env.GHOST_CONTENT_API_KEY,
-  version: "v5.0",
-});
-
 export async function getPosts() {
+  const config = useRuntimeConfig();
+
+  const api = new GhostContentAPI({
+    url: "https://truflation.ghost.io",
+    key: config.apiSecret,
+    version: "v5.0",
+  });
+
   return await api.posts
     .browse({
       limit: "all",
@@ -17,6 +19,13 @@ export async function getPosts() {
 }
 
 export async function getSinglePost(postSlug) {
+  const config = useRuntimeConfig();
+
+  const api = new GhostContentAPI({
+    url: "https://truflation.ghost.io",
+    key: config.apiSecret,
+    version: "v5.0",
+  });
   return await api.posts
     .read({
       slug: postSlug,
