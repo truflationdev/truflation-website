37f5e5eaea1b3220537fc2b036d1b85a59f29f2f
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-01-28 10:38:20 +0800

    google tag added and 404 page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <head>
+    <link
+      href="https://api.fontshare.com/v2/css?f[]=work-sans@400&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <div class="flex flex-col justify-center">
+    <div class="">
+      <Banner :isLight="false" />
+    </div>
+    <div class="flex flex-col items-center justify-center h-[60vh]">
+      <h1 class="text-[96px] font-medium">Page Not Found</h1>
+      <p class="text-[18px]">
+        It looks like this page doesn’t exist.
+        <a class="text-truflation-500" href="/"> Go back home</a> or
+        <a class="text-truflation-500" href="/contact"> contact us</a>
+        to report and issue.
+      </p>
+    </div>
+    <NewsLetter />
+    <FooterComp />
+  </div>
+</template>
+<style scoped></style>
