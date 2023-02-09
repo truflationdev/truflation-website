ed6472c100f5497441ee944f9f720883bde1dc19
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-02-09 09:33:06 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -21,9 +21,11 @@ const { data, refresh } = await useFetch(
   () => `http://worldtimeapi.org/api/ip`
 );
 
+console.log(data);
+
 onBeforeMount(() => {
   refresh();
-  main.updateCurrentTime(data);
+  main.updateCurrentTime(data._value.datetime);
 });
 
 async function fetchState() {
@@ -114,7 +116,7 @@ const testWarning = computed(() => {
                     <div class="w-11 h-6 bg-gray-200 peer-foc2us:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                     </label> -->
       </div>
-      <h1>{{ currentTime.datetime }}</h1>
+      <h1>{{ currentTime }}</h1>
       <P class="text-lg text-center lg:text-left"
         >{{ testWarning }} The {{ selectedCountry }} Inflation Rate by
         Truflation is
