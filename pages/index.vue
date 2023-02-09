1f8fd6d4ac9a5a0b2109fcb8864c95079fff2644
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-02-09 09:28:10 +0800

    more tests

--- pages/index.vue
+++ pages/index.vue
@@ -17,15 +17,13 @@ await useAsyncData(key, () => $fetch(`${defaultHost}/dashboard-data`)).then(
   }
 );
 
-await useAsyncData("time", () => $fetch(`http://worldtimeapi.org/api/ip`)).then(
-  (res) => {
-    console.log(res.data.value);
-    main.updateCurrentTime(res.data.value);
-  }
+const { data, refresh } = await useFetch(
+  () => `http://worldtimeapi.org/api/ip`
 );
 
-onUnmounted(() => {
-  refreshNuxtData("time");
+onBeforeMount(() => {
+  refresh();
+  main.updateCurrentTime(data);
 });
 
 async function fetchState() {
@@ -116,9 +114,10 @@ const testWarning = computed(() => {
                     <div class="w-11 h-6 bg-gray-200 peer-foc2us:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                     </label> -->
       </div>
+      <h1>{{ currentTime.datetime }}</h1>
       <P class="text-lg text-center lg:text-left"
-        >{{ testWarning }} The {{ currentTime.datetime }}
-        {{ selectedCountry }} Inflation Rate by Truflation is
+        >{{ testWarning }} The {{ selectedCountry }} Inflation Rate by
+        Truflation is
         <span class="font-extrabold text-lg">{{ keyMetrics.Inflation }}%</span>,
         <span
           class="font-bold"
