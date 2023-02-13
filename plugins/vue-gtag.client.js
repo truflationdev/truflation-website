dd8e86c7bd7a6ad9cd3896d308b27011e5fca05a
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-02-13 13:32:12 +0800

    more gtag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,11 +1,27 @@
-import VueGtag from "vue-gtag-next";
+// import VueGtag from "vue-gtag-next";
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const { gtagId } = useRuntimeConfig();
+
+//   nuxtApp.vueApp.use(VueGtag, {
+//     property: {
+//       id: gtagId,
+//     },
+//   });
+// });
+
+import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { gtagId } = useRuntimeConfig();
 
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: gtagId,
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      config: {
+        id: gtagId,
+      },
     },
-  });
+    nuxtApp.$router
+  );
 });
