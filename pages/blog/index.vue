980b1ac6046212e7d29c253d2c542f794e5add64
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-02-14 11:47:03 +0800

    more detail

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -16,11 +16,18 @@ const posts = await useFetch("/api/ghostPosts").data;
       property="og:image"
       content="https://res.cloudinary.com/djy27wpqd/image/upload/v1676338258/2023-02-14_09.00.20_cwqgmm.jpg"
     />
+
+    <Meta name="twitter:card" content="summary_large_image" />
+    <Meta name="twitter:site" content="@truflation" />
+    <Meta name="twitter:title" content="Blog | truflation" />
+    <Meta
+      name="twitter:description"
+      content="We've researched inflation, partnered with scientists, scoured the databases, and replaced outdated surveys with real-time market information."
+    />
     <Meta
       name="twitter:image"
       content="https://res.cloudinary.com/djy27wpqd/image/upload/v1676338258/2023-02-14_09.00.20_cwqgmm.jpg"
     />
-
     <link
       href="https://api.fontshare.com/v2/css?f[]=work-sans@400&display=swap"
       rel="stylesheet"
